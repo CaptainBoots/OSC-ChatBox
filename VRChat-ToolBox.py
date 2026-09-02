f0725d18604398bbd5eedae495c32068c0130fd0
Boots <boots@vrc-toolbox.local>
2026-09-02 19:52:42 +0100

    1.0.0 version reset

--- VRChat-ToolBox.py
+++ VRChat-ToolBox.py
@@ -77,7 +77,7 @@ from PySide6.QtWidgets import (
 # ═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════#
 
 # ─── App metadata / runtime state ──────────────────────────────────────────
-VERSION = "9.8.15"
+VERSION = "1.0.0"
 UPDATE_BRANCH = "main"           # Default selected update branch
 BETA_POPUP_SHOWN = False
 
