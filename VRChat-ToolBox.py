2da4130475dfe7eecf612259b959439b53a64f5a
Boots <boots@vrc-toolbox.local>
2026-09-02 05:54:57 +0100

    build: Downgrade compiler to PyInstaller 6.21.0 to eliminate the bootloader parent process security validation check entirely

--- VRChat-ToolBox.py
+++ VRChat-ToolBox.py
@@ -77,7 +77,7 @@ from PySide6.QtWidgets import (
 # ═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════#
 
 # ─── App metadata / runtime state ──────────────────────────────────────────
-VERSION = "9.8.10"
+VERSION = "9.8.11"
 UPDATE_BRANCH = "main"           # Default selected update branch
 BETA_POPUP_SHOWN = False
 
